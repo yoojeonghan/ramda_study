6bc5076a8c10a47e923aceab841d95888e4d7349
yoojeonghan <yoojeonghan@gmail.com>
2020-07-24 12:14:59 +0900

    add maulal

--- index.js
+++ index.js
@@ -417,8 +417,9 @@ async function a() {
 a();
 
 /**
- * 36. either(func a, func b, c)
- * c가 조건함수 a 또는 b를 만족하는지 여부를 판단하는 함수.
+ * 36. either(func a, func b, c), both(func a, func b, c)
+ * either은 c가 조건함수 a 또는 b를 만족하는지 여부를 판단하는 함수. ||
+ * both는 c가 조건함수 a와 b를 모두 만족하는지 여부를 판단하는 함수. && 
  */
 
 const eitherTest1 = x => x > 10;
@@ -428,6 +429,13 @@ const either = R.either(eitherTest1, eitherTest2);
 console.log(`36-1 :`, either(10));
 console.log(`36-2 :`, either(11.1));
 console.log(`36-3 :`, either(9.1));
+
+const bothTest1 = x => x > 10;
+const bothTest2 = x => x < 30;
+
+console.log(`36-4 :`, R.both(bothTest1, bothTest2)(15));
+console.log(`36-5 :`, R.both(bothTest1, bothTest2)(30));
+console.log(`36-6 :`, R.both(bothTest1, bothTest2)(null));
 
 /**
  * 37. gte(a, b)
@@ -485,7 +493,7 @@ console.log(`41-1 :`, R.identity(1));
 const useWith = R.useWith(R.mergeLeft, [R.objOf('number'), R.objOf('type')]);
 console.log(`42-1 :`, useWith(1, 2));
 
-const useWithTestObj = { 1:2, 3:4, 5:6, 7:8 };
+const useWithTestObj = { 1:2, 3:4, 5:6, 7:8 }; // -> [{ number: 1, type: 2 }, { number: 3, type: 4 }]
 const useWith2 = R.useWith(R.mergeLeft, [R.objOf('number'), R.objOf('type')]);
 console.log(`42-2 :`, R.map(R.apply(useWith2), R.toPairs(useWithTestObj)));
 
@@ -519,33 +527,268 @@ console.log(`44-1 :`, R.invoker(1, 'testfunc1')(1)(invokertestObj));
 console.log(`44-2 :`, R.invoker(3, 'testfunc2')(1,2,3)(invokertestObj));
 
 /**
- * 45. isNil
- * 
+ * 45. isNil(a)
+ * a가 null 혹은 undefined인지 확인하는 함수.
  */
 
-/**
- * 46. path
- * 
- */
+console.log(`45-1 :`, R.isNil(undefined));
+console.log(`45-2 :`, R.isNil(null));
+console.log(`45-3 :`, R.isNil(0));
 
 /**
- * split
- * it
- * defaultTo
- * inc
- * forEachObjindexed
- * add
- * clone
- * isEmpty
- * pickAll
- * view
- * substract
- * divide
- * flatten
- * of
- * values,
- * mapObjIndexed
- * both
- * is
- * 
+ * 46. path([a, b], c)
+ * Object c에서, c[a][b]를 반환한다.
  */
+
+const pathTestObj = [{ name: 'board1', episode: [{
+    name: 'episode1',
+    quest: [{
+        name: 'quest1'
+    }],
+}] }];
+
+console.log(`46-1 :`, R.path([0, 'episode', 0, 'quest'], pathTestObj));
+
+/**
+ * 47. split(a);
+ * 주어진 구분 기호를 기준으로. 문자열 a를 문자열 배열로 분할한다.
+ */
+
+const splitTestString = '1.12';
+console.log(`47-1 :`, R.split('.', splitTestString));
+
+/**
+ * 48. defaultTo(a, b)
+ * b가 null 또는 undefined일 경우 a를 반환하며, 그렇지 않은 경우 b를 반환한다.
+ */
+
+console.log(`48-1 :`, R.defaultTo('hahaha', null));
+console.log(`48-2 :`, R.defaultTo(null, null));
+console.log(`48-3 :`, R.defaultTo('hahaha', 2));
+
+/**
+ * 49. inc(a), dec(a)
+ * a를 1 증가시키고, 감소시킨다.
+ */
+
+console.log(`49-1 :`, R.inc(2), R.dec(2));
+
+/**
+ * 50. forEachObjIndexed(func a, b)
+ * 콜렉션 b를 순회하면서 b의 key와 value에 함수 a를 적용한다.
+ */
+
+const foreachObj = { first: 1 , second: 2 };
+R.forEachObjIndexed((value, key) => { foreachObj[key] = value + 1 }, foreachObj);
+console.log(`50-1 :`, foreachObj);
+
+/**
+ * 51. add(a, b), subtract(a, b)
+ * Number인 a와 b의 값을 더하고 뺀다.
+ */
+
+console.log(`51-1 :`, R.add(1, 2));
+console.log(`51-2 :`, R.subtract(1, 2));
+
+/**
+ * 52. clone(a)
+ * a에 대한 깊은 복사를 수행한다.
+ */
+
+const cloneTestObj = [ { a: 1, b: 1, c: 1 }, { a: 2, b: 2, c: 2 } ];
+const cloneTestObj2 = cloneTestObj;
+const cloneTestObj3 = R.clone(cloneTestObj);
+
+console.log(`52-1 :`, cloneTestObj, cloneTestObj2, cloneTestObj === cloneTestObj2);
+console.log(`52-2 :`, cloneTestObj, cloneTestObj3, cloneTestObj === cloneTestObj3);
+
+/**
+ * 53. isEmpty(a)
+ * a가 빈 값인지 여부를 판단한다.
+ */
+
+console.log(`53-1 :`, R.isEmpty(null));
+console.log(`53-2 :`, R.isEmpty(''));
+console.log(`53-3 :`, R.isEmpty([]));
+console.log(`53-4 :`, R.isEmpty({}));
+console.log(`53-5 :`, R.isEmpty(0));
+
+/**
+ * 54. pickAll([a, b], c)
+ * object c에서 string a, b이름의 키를 가진 객체를 key:value 형식으로 반환한다.
+ */
+
+const pickAllObj = { a: 1, b: 2, c: 3, d: 4 };
+console.log(`54-1 :`, R.pickAll(['a', 'b', 'e'], pickAllObj));
+
+/**
+ * 55. lens(a, b)
+ * lens는 어떤 객체의 내부 속성을 들여다보고 있는 돋보기이다. 렌즈를 이용해 해당 값을 조회 및 셋팅할 수 있다.
+ * a에 특정 객체에 대한 getter, b에 특정 객체에 대한 setter를 인자로 받아 lens를 생성한다.
+ */
+
+const lensObj = { a: 1, b: 2, c: 3, d: 4 };
+const getter = R.prop('a');
+const setter = R.assoc('a');
+
+const lens = R.lens(getter, setter);
+console.log(`55-1 :`, lens);
+
+/**
+ * 56. view(a, b)
+ * a에 lens를 넣고, b에 object를 넣어 해당 lens의 값을 조회한다.
+ */
+
+const view = R.view(lens, lensObj);
+console.log(`56-1 :`, view);
+
+/**
+ * 57. set(a, b, c)
+ * a에 lens를 넣고, c에 object를 넣은 뒤 해당 lens의 값을 b로 변경한다.
+ */
+
+const set = R.set(lens, 2, lensObj);
+console.log(`57-1 :`, set);
+
+/**
+ * 58. over(a, func b, c)
+ * a에 lens를 넣고, c에 object를 넣은 뒤 해당 렌즈에 함수 b를 적용한다.
+ */
+
+const over = R.over(lens, x => x * 10, lensObj);
+console.log(`58-1 :`, over);
+
+/**
+ * 59. lensIndex(a, b)
+ * b의 a번째 인덱스의 렌즈를 반환한다.
+ */
+
+const lensIndexArray = [0, 1, 2, 3, 4, 5];
+const lensIndex = R.lensIndex(1);
+console.log(`59-1 :`, R.view(lensIndex, lensIndexArray));
+
+/**
+ * 60. lensPath([a, b, c...], b)
+ * b의 [a, b, c ...]에 위치한 요소의 렌즈를 반환한다.
+ */
+
+const lensPathObj = [{
+    index: 1,
+    array: [{
+        value: '1-1',
+        }
+    ]
+}, {
+    index: 2,
+    array: [{
+        value: '2-1',
+    }
+    ]
+}];
+
+const lensPath = R.lensPath([0, 'array', 0, 'value']);
+console.log(`60-1 :`, JSON.stringify(R.view(lensPath, lensPathObj)));
+console.log(`60-2 :`, JSON.stringify(R.set(lensPath, 9999, lensPathObj)));
+console.log(`60-3 :`, JSON.stringify(R.over(lensPath, x => x + '99999', lensPathObj)));
+
+/**
+ * 61. lensProp(a, b)
+ * b의 a의 이름을 가진 요소의 렌즈를 반환한다.
+ */
+
+const lensPropObj = { a: 1, b: 1, c: 1, d: 1 };
+const lensProp = R.lensProp('c');
+console.log(`61-1 :`, R.view(lensProp, lensPropObj));
+console.log(`61-2 :`, R.set(lensProp, 2, lensPropObj));
+console.log(`61-3 :`, R.over(lensProp, x => x * 100, lensPropObj));
+
+/**
+ * 62. divide(a, b)
+ * a를 b로 나눈다.
+ */
+
+console.log(`62-1 :`, R.divide(1, 2));
+console.log(`62-2 :`, R.divide(0, 2));
+
+/**
+ * 63. flatten(a)
+ * n차원 배열 a를 1차원 배열로 변경한다.
+ */
+
+const flattenArray = [1,[2,[3,[4]]]];
+console.log(`63-1 :`, flattenArray, R.flatten(flattenArray));
+
+/**
+ * 64. of(a)
+ * a를 [a]로 변경한다.
+ */
+
+console.log(`64-1 :`, R.of(1));
+
+/**
+ * 65. values(a)
+ * object a에서 value만을 추출하여 배열 형식으로 반환한다.
+ */
+
+console.log(`65-1 :`, R.values({1: 2, 3: 4, 5: 6, 7: 8, 9: 10}));
+
+/**
+ * 66. mapObjIndexed(func a, b)
+ * b를 순회하며 b의 key, value 그리고 b에 a를 적용한다. 
+ */
+
+const mapObjIndexedObj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+R.mapObjIndexed((value, key, object) => { object[key] = key + value }, mapObjIndexedObj);
+console.log(`66-1 :`, mapObjIndexedObj);
+
+/**
+ * 67. paths([[a, b, c], [d, e, f]...], g)
+ * object g의 (a, b, c), (d, e, f)... 에 있는 경로에 위치한 값들을 배열 형태로 반환한다.
+ */
+
+const pathsObj = [{
+    index: 1,
+    object: [
+        {
+            value: 1-1
+        }
+    ]
+},{
+    index: 2,
+    object: [
+        {
+            value: 2-1
+        }
+    ]
+},{
+    index: 3,
+    object: [
+        {
+            value: 3-1
+        }
+    ]
+}];
+
+console.log(`67-1 :`, JSON.stringify(R.paths([[0, 'index'], [1, 'object', 0, 'value'], [2]], pathsObj)));
+
+/**
+ * 68. insert(a, b, c)
+ * array c의 a번째 인덱스에 b를 넣는다. 
+ */
+
+const insertArray = R.range(0, 10);
+console.log(`68-1 :`, R.insert(5, 20, insertArray));
+
+/**
+ * 69. or(a, b)
+ * a 또는 b가 true인지 확인한다.
+ */
+
+console.log(`69-1 :`, R.or(true, false));
+
+/**
+ * 70. not(a)
+ * a가 false인지 확인한다.
+ */
+
+console.log(`70-1 :`, R.not(false));
